1d8c21e489e54046184353c160ac3a4e354e1315
wictorkanerva <130720760+wictorkanerva@users.noreply.github.com>
2023-11-29 11:18:28 +0100

    Create ml_helper.py
    Init

---
+++ ml_helper.py
@@ -0,0 +1,36 @@
+# Tidbit: You could put a unciton like this into a script called "helper.py" and import it when you need it...
+
+import matplotlib.pyplot as plt
+def plot_loss_curves(history):
+    """
+    Returns seperate loss curves for training and validation metrics.
+    
+    Args:
+        history: TensorFlow History object
+        
+    Returns:
+        Plots of trainng/validation loss and accuracy metrics.
+    """
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+    
+    accuracy = history.history["accuracy"]
+    val_accuracy = history.history["val_accuracy"]
+    
+    num_epochs = range(len(history.history["loss"]))
+    
+    # Plot loss
+    plt.plot(num_epochs, loss, label="training_loss")
+    plt.plot(num_epochs, val_loss, label="val_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+    
+    # Plot accuracy
+    plt.figure()
+    plt.plot(num_epochs, accuracy, label="training_accuracy")
+    plt.plot(num_epochs, val_accuracy, label="val_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+    
